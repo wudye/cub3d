31825827dc231da3135d943de50836382df13d74
skoruf <chris.thaler89@gmail.com>
2024-04-22 18:31:16 +0200

    fixed out of bounds error init_rays

--- src/init_rays.c
+++ src/init_rays.c
@@ -56,12 +56,12 @@ float	calc_coordinate(t_ray *r, t_player *p, int horizontal, int x)
 int	check_hor_map(t_ray *r, char **map)
 {
 	if ((r->angle > 270 || r->angle < 90)
-		&& !check_bounds(r->x + 1, r->y, map)
+		&& !check_bounds(r->x + 1, r->y, map) && !check_bounds(r->x, r->y, map)
 		&& ((r->x == (int)r->x && map[(int)r->y][(int)r->x] == '1')
 		|| (r->x != (int)r->x && map[(int)r->y][(int)r->x + 1] == '1')))
 		return (1);
 	else if ((r->angle < 270 && r->angle > 90)
-		&& !check_bounds(r->x - 1, r->y, map)
+		&& !check_bounds(r->x - 1, r->y, map) && !check_bounds(r->x, r->y, map)
 		&& ((r->x == (int)r->x && map[(int)r->y][(int)r->x] == '1')
 		|| (r->x != (int)r->x && map[(int)r->y][(int)r->x - 1] == '1')))
 		return (1);
@@ -71,11 +71,13 @@ int	check_hor_map(t_ray *r, char **map)
 int	check_vert_map(t_ray *r, float x, float y, char **map)
 {
 	if (r->angle < 180 && !check_bounds(x, y + 1, map)
+		&& !check_bounds(x, y, map)
 		&& ((y == (int)y && map[(int)y][(int)x] == '1')
 		|| (y != (int)y && map[(int)y + 1][(int)x] == '1')))
 		return (1);
 
 	else if (r->angle > 180 && !check_bounds(x, y - 1, map)
+		&& !check_bounds(x, y, map)
 		&& ((y == (int)y && map[(int)y][(int)x] == '1')
 		|| (y != (int)y && map[(int)y - 1][(int)x] == '1')))
 		return (1);
