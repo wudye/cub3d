27cb2b47da084a54964bc43c493f2637961c25a8
Mingwei Wu <mwu@c3r7p4.42vienna.com>
2024-04-25 13:29:45 +0200

    finish makefie

--- Makefile
+++ Makefile
@@ -1,58 +1,44 @@
 
-NAME = cub3D
+#MLX
+INC=%%%%
+INCLIB=$(INC)/../lib
+LFLAGS = -L.. -lmlx -L$(INCLIB) -lXext -lX11 -lm
 
-INCL = includes
-
-LIB_DIR = libft
-
-LIB = $(LIB_DIR)/libft.a
-
-FILES =	main.c free_functions.c parse_main.c \
+CC     = cc
+CFLAGS = -Wall -Wextra -Werror -g
+NAME   = cub3d
+FILES  = main.c free_functions.c parse_main.c \
 		parse_utils.c parse_check_texture.c \
 		parse_check_texture_help.c check_map_numbers.c\
 		check_map_numbers_space.c check_map_numbers_wall.c \
 		render_mlx_main.c \
-		get_next_line.c get_next_line_utils.c 
-
-CFLILES = ${addprefix srcs/, ${FILES}}		
-OFILES = $(addprefix obj/,$(FILES:.c=.o))
-
-CFLAGS = -g -Wall -Wextra -Werror -I $(INCL) -I $(I_MLX) -I $(LIB_DIR)
-
-CC = cc
-
-I_MLX = minilibx_linux
-
-LFLAGS = -lXext -lX11 -lm
-
-MLX_NAME = $(I_MLX)/libmlx.a
-
-RM = rm -rf
-
-HEADER = $(INCL)/cub3D.h
+		get_next_line.c get_next_line_utils.c
+		# init_rays.c \
+		# math.c \
+		# render.c \
+		# render_utils.c \
+		# movement.c \
+		# utils.c \
 
 
-obj/%.o: srcs/%.c $(HEADER)
-	mkdir -p obj
-	$(CC) $(CFLAGS) -c $< -o $@
-
+CFILES = $(addprefix src/, $(FILES))
+OFILES = $(addprefix obj/, $(FILES:.c=.o))
+DEPS   = inc/cub3d.h inc/libft/libft.h
+obj/%.o	: src/%.c $(DEPS)
+	@mkdir -p obj
+	@$(CC) $(CFLAGS) -c $< -o $@
 all: $(NAME)
-
 $(NAME): $(OFILES)
-	@make -C $(LIB_DIR)
-	@make -C $(I_MLX)
-	$(CC) $(CFLAGS) $(OFILES) $(LIB) $(MLX_NAME) $(LFLAGS) -o $(NAME)
+	@make all -C ./inc/libft
+	@$(CC) -o $(NAME) $(OFILES) ./inc/libft/libft.a $(LFLAGS)
 
 clean:
-	make clean -C $(LIB_DIR)
-	make clean -C $(I_MLX)
-	$(RM) $(OFILES)
+	@make clean -C ./inc/libft
+	@rm -f $(OFILES)
+	@if [ -d "obj" ]; then rmdir obj; fi
 
-fclean:	clean
-	make fclean -C $(LIB_DIR)
-
-	$(RM) $(NAME)
-
+fclean: clean
+	@rm -f ./inc/libft/libft.a
+	@rm -f $(NAME)
 re: fclean all
-.PHONY: all bonus clean fclean re
-
+.PHONY:  all clean fclean re
---
+++ src/parse_check_texture.c
@@ -0,0 +1,148 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_check_texture.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwu <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 18:26:49 by mwu               #+#    #+#             */
+/*   Updated: 2024/04/22 18:37:57 by mwu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3d.h"
+
+static int	ctc(char *st, char *compare, t_var *var, int *color)
+{
+	char	**str;
+
+	str = ft_split(st, ' ');
+	if (!str)
+		error_malloc(var);
+	if (double_ft_len(str) != 2)
+	{
+		free_double_ptr(str);
+		return (1);
+	}
+	if (ft_strncmp(str[0], compare, ft_strlen(str[0]) + 1) != 0)
+		return (err_return_info("Error wrong color name", var), \
+						free_double_ptr(str), 1);
+	if (check_color_value(str[1], color) == -1)
+		return (free_double_ptr(str), error_malloc(var), 1);
+	if (check_color_value(str[1], color) == 1)
+		return (err_return_info("Error wrong color value 0-255", var), \
+						free_double_ptr(str), 1);
+	free_double_ptr(str);
+	return (0);
+}
+
+static int	check_load_texture(char *str, t_var *var, t_img *img)
+{
+	int	size;
+
+	img->img_ptr = mlx_xpm_file_to_image(var->mlx_init_ptr, str, &size, &size);
+	if (!img->img_ptr)
+		return (ft_putstr_fd("Error xpm file open failed\n", 2), 1);
+	img->img_addr = mlx_get_data_addr(img->img_ptr, \
+					&img->bits_per_pixel, &img->size_line, &img->endian);
+	if (!img->img_addr)
+		return (ft_putstr_fd("Error add xpm file open failed\n", 2), 1);
+	return (0);
+}
+
+static int	ctd(char *st, char *compare, t_var *var, t_img *vimg)
+{
+	char	**str;
+
+	str = ft_split(st, ' ');
+	if (!str)
+		error_malloc(var);
+	if (double_ft_len(str) != 2)
+	{
+		free_double_ptr(str);
+		return (1);
+	}
+	if (ft_strncmp(str[0], compare, ft_strlen(str[0]) + 1) != 0)
+		return (ft_putstr_fd("Error wrong direction\n", 2), \
+						free_double_ptr(str), 1);
+	if (check_load_texture(str[1], var, vimg) == 1)
+		return (free_var(var), free_double_ptr(str), 1);
+	free_double_ptr(str);
+	return (0);
+}
+
+/*
+// this part should discuss 
+// if the order of texture is mandatory 
+// should keep this one
+static int  check_texture_content(char *str, int i, t_var *var)
+{
+
+    if (i == 0)
+        return (check_texture_directions(str, "NO",var, var->north));
+    if (i == 1)
+        return (check_texture_directions(str, "SO", var, var->sourth));
+    if (i == 2)
+        return (check_texture_directions(str, "WE", var, var->west));
+    if (i == 3)
+        return (check_texture_directions(str, "EA", var, var->east));
+    if (i == 4)
+        return (check_texture_color(str, "F", var, var->floor));
+    if (i == 5)
+        return (check_texture_color(str, "C", var, var->ceil));
+    return (0);
+}
+
+int check_texture(t_var *var, char **texture)
+{
+    int i;
+
+    i = 0;
+    while (texture[i])
+    {
+        if (check_texture_content(texture[i], i, var) == 1)
+            return (err_return_info("Error texture format wrong", var));
+        i++;
+    }
+    return (0);
+}
+*/
+// if there is no order
+static int	check_texture_content(char *str, t_var *var)
+{
+	char	**temp;
+
+	temp = ft_split(str, ' ');
+	if (!temp)
+		return (error_malloc(var), 1);
+	if (double_ft_len(temp) != 2)
+		return (free_double_ptr(temp), \
+						err_return_info("Error texture format wrong", var), 1);
+	if (ft_strncmp(temp[0], "NO", 3) == 0)
+		return (free_double_ptr(temp), ctd(str, "NO", var, var->north));
+	else if (ft_strncmp(temp[0], "SO", 3) == 0)
+		return (free_double_ptr(temp), ctd(str, "SO", var, var->sourth));
+	else if (ft_strncmp(temp[0], "WE", 3) == 0)
+		return (free_double_ptr(temp), ctd(str, "WE", var, var->west));
+	else if (ft_strncmp(temp[0], "EA", 3) == 0)
+		return (free_double_ptr(temp), ctd(str, "EA", var, var->east));
+	else if (ft_strncmp(temp[0], "F", 2) == 0)
+		return (free_double_ptr(temp), ctc(str, "F", var, var->floor));
+	else if (ft_strncmp(temp[0], "C", 2) == 0)
+		return (free_double_ptr(temp), ctc(str, "C", var, var->ceil));
+	return (free_double_ptr(temp), err_return_info("Error in texture", var));
+}
+
+int	check_texture(t_var *var, char **texture)
+{
+	int	i;
+
+	i = 0;
+	while (texture[i])
+	{
+		if (check_texture_content(texture[i], var) == 1)
+			return (1);
+		i++;
+	}
+	return (0);
+}
