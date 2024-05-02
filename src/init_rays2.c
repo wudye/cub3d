157ff0ec4e70b13f3fdc2561803ffa50a1c80382
skoruf <chris.thaler89@gmail.com>
2024-05-02 07:44:53 +0200

    changed map check init_rays2

--- src/init_rays2.c
+++ src/init_rays2.c
@@ -21,34 +21,36 @@ static int	check_map_pos(float x, float y, char **map)
 
 int	check_hor_map(t_ray *r, char **map)
 {
-	if (r->angle < 90 && (!check_map_pos(r->x, r->y, map)
-		|| (r->y && check_map_pos(r->x, r->y - 1, map))))
-		return (0);
-	else if ((r->angle > 90 && r->angle < 180) && (!check_map_pos(r->x, r->y, map)
-		|| (r->y && check_map_pos(r->x, r->y - 1, map))))
-		return (0);
-	else if ((r->angle > 180 && r->angle < 270) && (!check_map_pos(r->x, r->y, map)
-		|| check_map_pos(r->x, r->y + 1, map)))
-		return (0);
-	else if (r->angle > 270 && (!check_map_pos(r->x, r->y, map)
-		|| check_map_pos(r->x, r->y + 1, map)))
-		return (0);
-	return (1);
+	if (r->angle < 90 && check_map_pos(r->x, r->y - 1, map))
+		return (1);
+	else if ((r->angle > 90 && r->angle < 180)
+		&& (check_map_pos(r->x, r->y - 1, map)))
+		return (1);
+	else if ((r->angle > 180 && r->angle < 270)
+		&& ((fabs(r->p->y - r->y) > 1 || !check_map_pos(r->x, r->p->y, map)) 
+			&& check_map_pos(r->x, r->y, map)))
+		return (1);
+	else if ((r->angle > 270 && r->angle < 360)
+		&& ((fabs(r->p->y - r->y) > 1 || !check_map_pos(r->x, r->p->y, map)) 
+			&& check_map_pos(r->x, r->y, map)))
+		return (1);
+	return (0);
 }
 
 int	check_vert_map(t_ray *r, float x, float y, char **map)
 {
-	if (r->angle < 90 && (!check_map_pos(x, y, map)
-		|| (x && check_map_pos(x - 1, y, map))))
-		return (0);
-	else if ((r->angle > 90 && r->angle < 180) && (!check_map_pos(x, y, map)
-		|| check_map_pos(x + 1, y, map)))
-		return (0);
-	else if ((r->angle > 180 && r->angle < 270) && (!check_map_pos(x, y, map)
-		|| check_map_pos(x + 1, y, map)))
-		return (0);
-	else if (r->angle > 270 && (!check_map_pos(x, y, map)
-		|| (x && check_map_pos(x - 1, y, map))))
-		return (0);
-	return (1);
+	if ((r->angle > 0 && r->angle <= 90)
+		&& (fabs(r->p->x - x) > 1 || !check_map_pos(r->p->x, y, map))
+		&& (check_map_pos(x, y, map) || check_map_pos(x - 1, y, map)))
+		return (1);
+	else if ((r->angle > 90 && r->angle <= 180)
+		&& (fabs(r->p->x - x) > 1 || !check_map_pos(r->p->x, y, map))
+		&& check_map_pos(x, y, map))
+		return (1);
+	else if ((r->angle > 180 && r->angle <= 270) && check_map_pos(x, y, map))
+		return (1);
+	else if ((r->angle > 270 || r->angle == 0)
+		&& (check_map_pos(x, y, map) || check_map_pos(x - 1, y, map)))
+		return (1);
+	return (0);
 }
--- src/render.c
+++ src/render.c
@@ -11,35 +11,14 @@
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
-/*
-static int	check_load_texture(char *str, t_var *var, t_img1 *img)
-{
-	
-(void)str;
-(void)var;
-(void)img;
-	int	size;
-	//testing the textures, remove function later
-	img->img_ptr = mlx_xpm_file_to_image(var->mlx_init_ptr, str, &size, &size);
-	if (!img->img_ptr)
-		return (ft_putstr_fd("Error xpm file open failed\n", 2), 1);
-	img->img_addr = (int *)mlx_get_data_addr(img->img_ptr, \
-					&img->bits_per_pixel, &img->size_line, &img->endian);
-	if (!img->img_addr)
-		return (ft_putstr_fd("Error add xpm file open failed\n", 2), 1);
-	return (0);
-}
-*/
+
 static void	draw_pixels(t_var *var, int x, int h, int w)
 {
 	int	x2;
 	int	y;
 	int	draw_h;
 	t_img	*img;
-	
-	//testing texture stuff
-	//t_img1 img_test;
-	//check_load_texture("teapot.xpm", var, &img_test);
+
 	img = var->img;
 	//later use wall textures
 	int test_rgb;
@@ -50,7 +29,6 @@ static void	draw_pixels(t_var *var, int x, int h, int w)
 	else if (var->r.vert && var->r.angle > 90 && var->r.angle < 270)
 		test_rgb = create_trgb(0, 255, 128, 26);
 	else
-		//test_rgb = img_test.img_addr[174];
 		test_rgb = create_trgb(0, 255, 0, 255);
 	y = IMG_H / 2 + h / 2;
 	draw_h = 0;
